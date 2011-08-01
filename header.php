42f47078a580a9532af75655351498ade1df4f1d
Brina Lee <leebrina@gmail.com>
2011-07-31 23:02:45 -0700

    Adding header, index, and footer pages.

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Mediation Center</title>
+        <link rel="shortcut icon" href="images/favicon.ico" >
+        <link href="css/site_layout.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <section>
+            <div id="container">
+                <header>
+                    <div id="header">
+                        <div id="logo">
+                        </div>
+                        <nav>
+                            <div id="main-nav">
+                                <ul>
+				    <li><a href="../">home</a></li>
+				    <li><a href="../about.php">about</a></li>
+				    <li><a href="../donations.php">donations</a></li>
+				    <li><a href="../services.php">services</a></li>
+				    <li><a href="../events.php">events</a></li>
+                                    <li><a href="../contact.php">contact</a></li>
+				</ul>
+                            </div>
+                        </nav>
+                    </div>
+                </header>
+            
+            
+            
